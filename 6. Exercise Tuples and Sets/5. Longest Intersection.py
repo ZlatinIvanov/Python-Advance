74c99039215d89fdccf442608b8944b1be038130
ZlatinIvanov <zlatin89@gmail.com>
2023-05-16 07:33:09 +0300

    Update 5. Longest Intersection.py

--- 6. Exercise Tuples and Sets/5. Longest Intersection.py
+++ 6. Exercise Tuples and Sets/5. Longest Intersection.py
@@ -3,8 +3,8 @@ longest_intersection = set()
 for _ in range(int(input())):
     first_data, second_data = [el.split(",") for el in input().split("-")]
 
-    first_range = set(range(int(first_data[0]), int(first_data[1] + 1)))
-    second_range = set(range(int(second_data[0]), int(second_data[1] + 1)))
+    first_range = set(range(int(first_data[0]), int(first_data[1]) + 1))
+    second_range = set(range(int(second_data[0]), int(second_data[1]) + 1))
 
     intersection = first_range.intersection(second_range)
 
