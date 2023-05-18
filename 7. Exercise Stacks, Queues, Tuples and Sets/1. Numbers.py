caf91a47fee25f49a9b325ac1c3fac3329c02bb9
ZlatinIvanov <zlatin89@gmail.com>
2023-05-18 07:15:14 +0300

    Update 1. Numbers.py

--- 7. Exercise Stacks, Queues, Tuples and Sets/1. Numbers.py
+++ 7. Exercise Stacks, Queues, Tuples and Sets/1. Numbers.py
@@ -1,4 +1,18 @@
-fist_sequence = int(input().split())
-second_sequence = int(input().split())
-n = int(input())
+first_sequence = set(int(x) for x in input().split())
+second_sequence = set(int(x) for x in input().split())
+for _ in range(int(input())):
+    first_command, second_command, *data = input().split()
+    command = first_command + " " + second_command
+    if command == "Add First":
+        [first_sequence.add(int(el)) for el in data]
+    elif command == "Add Second":
+        [second_sequence.add(int(el)) for el in data]
+    elif command == "Remove First":
+        [first_sequence.discard(int(el)) for el in data]
+    elif command == "Remove Second":
+        [second_sequence.discard(int(el)) for el in data]
+    else:
+        print(first_sequence.issubset(second_sequence) or second_sequence.issubset(first_sequence))
 
+print(*sorted(first_sequence), sep=", ")
+print(*sorted(second_sequence), sep=", ")
