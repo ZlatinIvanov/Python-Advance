910b3e873057bb360c2edd400ec2e8c7b7585dca
ZlatinIvanov <zlatin89@gmail.com>
2023-05-18 07:15:51 +0300

    Create 5. Santa's Present Factory.py

---
+++ 7. Exercise Stacks, Queues, Tuples and Sets/5. Santa's Present Factory.py
@@ -0,0 +1,28 @@
+from collections import deque
+
+materials = deque(int(x) for x in input().split())
+magic_levels = deque(int(x) for x in input().split())
+
+crafted = []
+presents = {
+    150: "Doll",
+    250: "Wooden train",
+    300: "Teddy bear",
+    400: "Bicycle",
+}
+
+while materials and magic_levels:
+    material = materials.pop() if magic_levels or not materials[0] else 0
+    magic_level = magic_levels.popleft() if materials or not magic_levels[0] else 0
+
+    if not magic_level:
+        continue
+
+    product = material * magic_level
+
+    if presents.get(product):
+        crafted.append(presents[product])
+    elif product < 0:
+        materials.append(material + magic_level)
+    elif product > 0:
+        materials.append(material + 15)
