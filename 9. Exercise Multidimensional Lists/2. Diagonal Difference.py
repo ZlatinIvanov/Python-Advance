68ec8fdcaf1c2065523be74888a9a4ee69b016d0
ZlatinIvanov <zlatin89@gmail.com>
2023-05-29 07:31:33 +0300

    Update 2. Diagonal Difference.py

--- 9. Exercise Multidimensional Lists/2. Diagonal Difference.py
+++ 9. Exercise Multidimensional Lists/2. Diagonal Difference.py
@@ -1,3 +1,11 @@
-n = int(input())
+num = int(input())
+matrix = [[int(n) for n in input().split()] for row in range(num)]
 
-matrix = [[int(x) for x in input().split(" ")] for _ in range(n)]
+primary_sum = 0
+secondary_sum = 0
+
+for i in range(num):
+    primary_sum += matrix[i][i]
+    secondary_sum += matrix[i][num - i - 1]
+
+print(abs(primary_sum - secondary_sum))
