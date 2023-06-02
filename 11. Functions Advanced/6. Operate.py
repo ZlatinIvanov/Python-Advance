dc0c103d8a6f897edf40cae76289a2e5ef26d515
ZlatinIvanov <zlatin89@gmail.com>
2023-06-02 07:59:17 +0300

    Create 6. Operate.py

---
+++ 11. Functions Advanced/6. Operate.py
@@ -0,0 +1,32 @@
+from functools import reduce
+
+def operate(operator, *args):
+    # def sum_nums():
+    #     return sum(args)
+    #
+    # def subtract():
+    #     res = 0
+    #     for num in args:
+    #         res -= num
+    #     return res
+    #
+    # def multiply():
+    #     res = 1
+    #     for num in args:
+    #         res *= num
+    #     return res
+
+    # def divide():
+    #     res = 0
+    #     for num in args:
+    #         res /= num
+    #     return res
+    return reduce(lambda a,b: eval(f"{a}{operator}{b}"), args)
+    # if operator == "+":
+    #     return reduce(lambda a, b: a+b, args)
+    # elif operator == "-":
+    #     return reduce(lambda a, b: a-b, args)
+    # elif operator == "*":
+    #     return reduce(lambda a, b: a*b, args)
+    # else:
+    #     return reduce(lambda a, b: a/b, args)
