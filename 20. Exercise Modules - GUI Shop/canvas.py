fc0dedf4a57d36ad3ce4bf69bae7ffed2aa51ad9
ZlatinIvanov <zlatin89@gmail.com>
2023-06-23 06:16:17 +0300

    Create canvas.py

---
+++ 20. Exercise Modules - GUI Shop/canvas.py
@@ -0,0 +1,23 @@
+from tkinter import Tk, Canvas
+
+
+def create_root():
+    root = Tk()
+
+    root.title("GUI Shop")
+    root.geometry(f"700x600")
+    root.resizable(False, False)
+
+    return root
+
+
+def create_frame():
+    frame = Canvas(root, width=700, height=700)
+    frame.grid(row=0, column=0)
+
+    return frame
+
+
+root = create_root()
+
+frame = create_frame()
